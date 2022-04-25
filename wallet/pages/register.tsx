6c2486248a832133b2c4380b7c84a14d3ecded8d
Arcticae <tomekgsd@gmail.com>
2022-04-25 18:54:37 +0200

    Add registration screen

---
+++ wallet/pages/register.tsx
@@ -0,0 +1,16 @@
+import fs from "fs";
+import RegisterFlow from "../flows/MainFlow";
+import { NextPage } from "next";
+
+interface RegisterPageProps {
+  accountContract: string;
+}
+
+export const getStaticProps = () => {
+  const accountContract = fs.readFileSync('../haskell-contract/build/main.json').toString(); // FIXME
+  return { props: { accountContract } };
+}
+
+const RegisterPage: NextPage<RegisterPageProps> = ({ accountContract }) => <RegisterFlow compiledAccountContract={accountContract} />
+
+export default RegisterPage;
