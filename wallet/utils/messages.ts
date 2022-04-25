a54d0c8614d6455db25cd5551224615151c60bb0
Jakub Ptak <jakub.ptak@swmansion.com>
2022-04-25 12:15:09 +0200

    Add encode/decode for messages

--- wallet/utils/messages.ts
+++ wallet/utils/messages.ts
@@ -16,3 +16,6 @@ export const useListener = (name: string, handler: MessageHandler) => {
         return () => window.removeEventListener("message", handler)
     }, [name]);
 }
+
+export const encode = (obj: Record<string, any>): string => Buffer.from(JSON.stringify(obj)).toString("base64");
+export const decode = (value: string): Record<string, any> => JSON.parse(Buffer.from(value, 'base64').toString());
